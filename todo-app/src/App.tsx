4711f2e844ea9c572a4b501f3c15a9f3c4845e21
songha <dddtttt000@gmail.com>
2022-09-08 19:20:24 +0900

    install react app

---
+++ todo-app/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>TODO LIST</p>
+      </header>
+      <p>add</p>
+    </div>
+  );
+}
+
+export default App;
